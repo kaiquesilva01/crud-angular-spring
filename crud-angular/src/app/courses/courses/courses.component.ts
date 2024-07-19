a6bcadef3ee3ba37eaff3e0d6da8fe565ea6d170
Kaique <kaique.andruid@gmail.com>
2024-07-18 21:52:42 -0300

    feat: mat-card

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -8,7 +8,8 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent {
     courses: Course[] = [
-      { _id: '1', name:'Angular', category: 'front-end'}
+      { _id: '1', name:'Angular', category: 'front-end'},
+      { _id: '2', name:'Java', category: 'back-end'}
     ];
     displayedColumns = ['name', 'category']
 }
