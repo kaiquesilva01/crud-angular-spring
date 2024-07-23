4fecd05bcf2d5854681d0ccf3cf74a26e4b2cae7
Kaique <kaique.andruid@gmail.com>
2024-07-22 23:36:18 -0300

    feat: http get courses

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -8,7 +9,7 @@ import { CoursesService } from '../services/courses.service';
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
-    courses: Course[] = [];
+    courses: Observable<Course[]>;
     displayedColumns = ['name', 'category']
 
     //coursesService: CoursesService
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name:'Angular', category: 'front-end'},
-      { _id: '2', name:'Java', category: 'back-end'}
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      tap(x => console.log(x))
+    );
   }
 }
